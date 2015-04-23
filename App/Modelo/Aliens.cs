58b388039d527e5f4229fd697011d9109f4cfe19
IbraMorelo <z.ibrahimovic1995@gmail.com>
2015-04-23 13:14:02 -0500

    Segunda Modificación

---
+++ App/Modelo/Aliens.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace App.Modelo
+{
+    public class Aliens : Personajes, IFicha
+    {
+        private string planeta;
+
+        public string Planeta
+        {
+            get { return planeta; }
+            set { planeta = value; }
+        }
+        public Aliens()
+            : base()
+        {
+
+        }
+        public Aliens(string sexo, string complexion, string nombre, string planeta)
+            : base(sexo, complexion, nombre)
+        {
+            this.planeta = planeta;
+
+        }
+        public override string ToString()
+        {
+            return base.ToString()+
+                               "\nPlaneta de Origen: " + this.planeta;
+        }
+        public override int GetHashCode()
+        {
+            return this.ToString().GetHashCode();
+        }
+        public override bool Equals(object obj)
+        {
+            Aliens o = (Aliens)obj;
+            bool result = false;
+
+            if (base.Equals(o) && (this.planeta == o.planeta))
+
+                result = true;
+
+            return result;
+        }
+
+    }
+}
---
+++ App/Modelo/Caracterizacion.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace App.Modelo
+{
+    public abstract class Caracterizacion
+    {
+        public Caracterizacion()
+        {
+          
+        }
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj);
+        }
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+    }
+}
